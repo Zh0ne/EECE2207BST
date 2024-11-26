61dd5290c9298ead26d28b459a36b279fd65574b
Zh0ne <Butler9396@hotmail.com>
2024-11-26 11:36:21 -0600

    Add files via upload

---
+++ Node.m
@@ -0,0 +1,18 @@
+% Node.m
+classdef Node
+    properties
+        data
+        left
+        right
+    end
+    
+    methods
+        function obj = Node(node_value)
+            if margin > o 
+                obj.data = node_value;
+                obj.left = [];
+                obj.right = [];
+            end 
+        end
+    end
+end
